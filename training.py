40976030d64f4baa9f893d875041e4d14ac6d2e5
tf-test <33952751+tf-test@users.noreply.github.com>
2017-11-24 16:59:07 +0800

    training.py

---
+++ training.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+w1 = tf.placeholder("float", name="w1")
+w2 = tf.placeholder("float", name="w2")
+b1 = tf.Variable(2.0, name="bias")
+feed_dict = {w1:4,w2:8}
+w3 = tf.add(w1,w2)
+w4 = tf.multiply(w3,b1,name="op_to_restore")
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+print(sess.run(w4,feed_dict))
+saver.save(sess, "mymodel/abc.ckpt")
